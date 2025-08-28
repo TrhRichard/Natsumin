3559990f0ec244452e6036774f5da96153ff4425
Richard <62023434+TrhRichard@users.noreply.github.com>
2025-08-28 05:12:04 +0300

    Updated schema for Master and Season
    Master db will now include a user_aliases table for username changes and other stuff like that.
    Season schema has been updated to have way less user data but will instead have a user_id that can be used to fetch data from master database.

--- contracts/Master.sql
+++ contracts/Master.sql
@@ -4,34 +4,37 @@ PRAGMA foreign_keys = ON;
 -- Users table
 CREATE TABLE IF NOT EXISTS users (
 	id         INTEGER PRIMARY KEY AUTOINCREMENT, 
-	discord_id INTEGER NOT NULL UNIQUE,          
+	discord_id INTEGER UNIQUE,    
 	username   TEXT NOT NULL,
 	rep        TEXT NOT NULL,
 	gen        INTEGER -- since we include aria badges theres a high chance some people in there might not be in contracts so no gen
 );
 
+-- User aliases table, allows to find a user from a past username
+CREATE TABLE IF NOT EXISTS user_aliases (
+	username TEXT PRIMARY KEY,
+	user_id  INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
 -- Legacy leaderboard, no plans for new leaderboard since it's still wip according to the sheet
-CREATE TABLE IF NOT EXISTS legacy (
-	user_id INTEGER PRIMARY KEY NOT NULL,         
-	exp     INTEGER NOT NULL,
-	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS legacy_leaderboard (
+	user_id INTEGER PRIMARY KEY NOT NULL REFERENCES users(id) ON DELETE CASCADE,         
+	exp     INTEGER NOT NULL
 );
 
 -- Badge definitions, urls are done manually cause google sucks ass
 CREATE TABLE IF NOT EXISTS badges (
-	id      	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-	name    	TEXT NOT NULL,
+	id          INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+	name        TEXT NOT NULL,
 	description TEXT NOT NULL,
-	artist  	TEXT NOT NULL,
-	url     	TEXT NOT NULL, -- URL to image/gif/video/etc
+	artist      TEXT NOT NULL,
+	url         TEXT NOT NULL, -- URL to image/gif/video/etc
 	type        TEXT NOT NULL DEFAULT 'contracts'
 );
 
 -- Which user owns which badges, so that I don't have to use json to keep track
 CREATE TABLE IF NOT EXISTS user_badges (
-	user_id		INTEGER NOT NULL,                     
-	badge_id	INTEGER NOT NULL,
-	PRIMARY KEY (user_id, badge_id),
-	FOREIGN KEY (user_id)  REFERENCES users(id) ON DELETE CASCADE,
-	FOREIGN KEY (badge_id) REFERENCES badges(id) ON DELETE CASCADE
+	user_id  INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,                     
+	badge_id INTEGER NOT NULL REFERENCES badges(id) ON DELETE CASCADE,
+	PRIMARY KEY (user_id, badge_id)
 );
--- contracts/Season.sql
+++ contracts/Season.sql
@@ -1,31 +1,29 @@
 PRAGMA journal_mode=WAL;
+PRAGMA foreign_keys = ON;
 
 CREATE TABLE IF NOT EXISTS users (
-	username TEXT PRIMARY KEY NOT NULL UNIQUE,
-	status INTEGER NOT NULL,
-	kind INTEGER NOT NULL,
-	discord_id INTEGER,
-	rep TEXT,
-	contractor TEXT,
-	list_url TEXT,
-	veto_used BOOLEAN,
-	accepting_manhwa BOOLEAN,
-	accepting_ln BOOLEAN,
-	preferences TEXT,
-	bans TEXT
+	user_id            INTEGER PRIMARY KEY, -- User id from master.db
+	status             INTEGER NOT NULL,
+	primary_contractor TEXT,
+	list_url           TEXT,
+	veto_used          BOOLEAN,
+	accepting_manhwa   BOOLEAN,
+	accepting_ln       BOOLEAN,
+	preferences        TEXT,
+	bans               TEXT
 );
 
 CREATE TABLE IF NOT EXISTS contracts (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	name TEXT NOT NULL,
-	type TEXT NOT NULL,
-	kind INTEGER NOT NULL,
-	status INTEGER NOT NULL,
-	contractee TEXT NOT NULL,
-	optional BOOLEAN,
-	contractor TEXT,
-	progress TEXT,
-	rating TEXT,
+	id         INTEGER PRIMARY KEY AUTOINCREMENT,
+	name       TEXT NOT NULL,
+	type       TEXT NOT NULL,
+	kind       INTEGER NOT NULL,
+	status     INTEGER NOT NULL,
+	user_id    INTEGER NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+	contractor TEXT NOT NULL,
+	optional   BOOLEAN DEFAULT FALSE,
+	progress   TEXT,
+	rating     TEXT,
 	review_url TEXT,
-	medium TEXT
+	medium     TEXT
 );
