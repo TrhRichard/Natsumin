112f1be644dde8fe59b732cc5e251f04fb80376c
Richard <62023434+TrhRichard@users.noreply.github.com>
2025-11-09 16:30:09 +0200

    Create Giveaway.sql

---
+++ assets/schemas/Giveaway.sql
@@ -0,0 +1,37 @@
+PRAGMA journal_mode = WAL;
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE IF NOT EXISTS giveaways (
+	message_id	INTEGER PRIMARY KEY,
+	channel_id	INTEGER NOT NULL,
+	guild_id	INTEGER NOT NULL,
+	author_id	INTEGER NOT NULL,
+	reward		TEXT NOT NULL,
+	winners		INTEGER NOT NULL CHECK(winners > 0),
+	ends_at		INTEGER NOT NULL CHECK(ends_at > created_at),
+	created_at	INTEGER NOT NULL DEFAULT (strftime('%s','now')),
+	ended		BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS role_requirements (
+	giveaway_id INTEGER NOT NULL REFERENCES giveaways(message_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	role_id 	INTEGER NOT NULL,
+	PRIMARY KEY (giveaway_id, role_id)
+);
+
+CREATE TABLE IF NOT EXISTS users_entered (
+	giveaway_id	INTEGER NOT NULL REFERENCES giveaways(message_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	user_id		INTEGER NOT NULL,
+	PRIMARY KEY	(giveaway_id, user_id)
+);
+
+CREATE TABLE IF NOT EXISTS winners (
+	giveaway_id 	INTEGER NOT NULL REFERENCES giveaways(message_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	winner_index	INTEGER NOT NULL CHECK(winner_index > 0),
+	user_id			INTEGER NOT NULL,
+	PRIMARY KEY (giveaway_id, winner_index),
+	UNIQUE (giveaway_id, user_id)
+);
+
+CREATE INDEX IF NOT EXISTS giveaways_ends_at ON giveaways(ends_at);
+CREATE INDEX IF NOT EXISTS users_entered_user_id ON users_entered(user_id);
