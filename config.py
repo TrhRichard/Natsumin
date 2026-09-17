f2df4f68dc9a848f473226b4e38999f182b99df5
Richard <62023434+TrhRichard@users.noreply.github.com>
2026-09-17 18:26:24 +0300

    feat: Move id lists to .env
    Been bothering me for a while to modify guild ids just to test locally, also have logging channel in the .env as well

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 
+import json
 import os
 
 load_dotenv(".env")
@@ -9,11 +10,12 @@ BOT_PREFIX = os.getenv("DISCORD_PREFIX", "%")
 IS_PRODUCTION = os.getenv("PRODUCTION", "false").lower() == "true"
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+LOGGING_CHANNEL_ID = int(os.getenv("LOGGING_CHANNEL_ID", -1))
 
-EDITOR_IDS = (546659584727580692, 448318227219742720, 243880818651430912, 133087204980424704)
-GUILD_IDS = (974344703266938911, 1272945842319261746, 994071728017899600)
-OWNER_IDS = (546659584727580692,)
-CONTRIBUTOR_IDS = (961063229168164864,)
+EDITOR_IDS: list[int] = json.loads(os.getenv("EDITOR_IDS", "[]"))
+GUILD_IDS: list[int] = json.loads(os.getenv("GUILD_IDS", "[]"))
+OWNER_IDS: list[int] = json.loads(os.getenv("OWNER_IDS", "[]"))
+CONTRIBUTOR_IDS: list[int] = json.loads(os.getenv("CONTRIBUTOR_IDS", "[]"))
 REPOSITORY_URL = "https://github.com/TrhRichard/Natsumin"
 
 DISABLED_EXTENSIONS = ()
